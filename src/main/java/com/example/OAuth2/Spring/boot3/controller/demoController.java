a7dc105dee504531b9aec7bccdeb37dc695650d6
Sarthak Aggarwal <102408244+Sarthak1008@users.noreply.github.com>
2024-04-05 00:26:15 +0530

    Add files via upload

---
+++ src/main/java/com/example/OAuth2/Spring/boot3/controller/demoController.java
@@ -0,0 +1,17 @@
+package com.example.OAuth2.Spring.boot3.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/demo")
+public class demoController {
+
+    @GetMapping
+    public ResponseEntity<String> s() {
+        return ResponseEntity.ok("Hello OAuth2");
+    }
+
+}
---
+++ src/test/java/com/example/OAuth2/Spring/boot3/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OAuth2.Spring.boot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
